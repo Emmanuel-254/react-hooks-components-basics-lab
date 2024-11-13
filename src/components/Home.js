fd3d8d58e818145e52a020bb1ea81a46d0c312e1
Emmanuel Tikan <emmanueltikan@gmail.com>
2024-11-13 13:52:40 +0300

    successful

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+// src/components/Home.js
+import React from "react";
+
+function Home() {
+  return (
+    <div id="home">
+      <h1>Welcome to my Portfolio!</h1>
+      <p>This is the homepage.</p>
+    </div>
+  );
+}
+
+export default Home;
